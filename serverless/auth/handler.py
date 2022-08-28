df8565ac8e0b4bd1e23dff989cf3e1a953f79589
Paolo Rechia <paolorechia@gmail.com>
2022-08-28 23:11:09 +0200

    Redirect hello world

---
+++ serverless/auth/handler.py
@@ -0,0 +1,13 @@
+import json
+
+def hello(event, context):
+    print("Got event :)")
+    print(event)
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
