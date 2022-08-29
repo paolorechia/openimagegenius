ca5c9ef081b11640353a6524d3236b48c42e1b8b
paolorechia <paolorechia@gmail.com>
2022-08-29 20:26:25 +0200

    Fix test authorizer

--- serverless/openimage-lambda/test_authorizer.py
+++ serverless/openimage-lambda/test_authorizer.py
@@ -1,5 +1,6 @@
 
 
 def handler(event, context):
-    print(event)
-    return {"statusCode": 200, "message": "You're authorized :)"}
+    print("Event", event)
+    print("contex", context)
+    return {"statusCode": 200, "body": "You're authorized :)"}
