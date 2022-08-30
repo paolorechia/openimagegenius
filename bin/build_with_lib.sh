da37310c4362a86344063ca9da208039a3a6b236
Paolo Rechia <paolorechia@gmail.com>
2022-08-30 13:37:40 +0200

    Fix build script

--- bin/build_with_lib.sh
+++ bin/build_with_lib.sh
@@ -3,14 +3,21 @@
 lib_name="openimage_backend_lib"
 origin_folder="$(pwd)"
 output_folder="$(pwd)/manual_dist"
-# echo "Saving to $output_folder"
+echo "Saving to $output_folder"
 
-# lib_folder="/Users/paolorechia/dev/openimagegenius/openimage_backend_lib"
+lib_folder="../../openimage_backend_lib"
 
-# echo "Changing to $lib_folder"
-# cd $lib_folder
-# python3 -m build --outdir $output_folder
+echo "Changing to $lib_folder"
+cd $lib_folder
+python3 -m build --outdir $output_folder
 
-# echo "Returning to origin folder $origin_folder"
-# cd $origin_folder
+echo "Returning to origin folder $origin_folder"
+cd $origin_folder
+
+echo "Trimming requirements.txt"
+sed -i.bak "/${lib_name}/d" requirements.txt
+echo "Downloading requirements.txt"
 pip download -r requirements.txt --dest $output_folder
+echo "Adding lib name back to requirements.txt"
+
+echo $lib_name >> requirements.txt
