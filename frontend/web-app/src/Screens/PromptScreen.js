2b027ce11f28311a8dd1d8ef1620d5881f383f3b
paolorechia <paolorechia@gmail.com>
2022-09-02 14:36:53 +0200

    Very basic layout for PromptScreen

--- frontend/web-app/src/Screens/PromptScreen.js
+++ frontend/web-app/src/Screens/PromptScreen.js
@@ -1,8 +1,53 @@
+import { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/system';
+
 
 export default function PromptScreen() {
+
+    const [prompt, setPrompt] = useState("")
+
+    function submitPrompt() {
+        if (!prompt) {
+            console.error("Empty prompt!")
+            return;
+        }
+        console.log("Your prompt is: ", prompt)
+        console.log("Submitting...")
+    }
+
+    function updatePrompt(event) {
+        setPrompt(event.target.value)
+    }
+
     return (
-        <div>
-            Hello Prompt Screen!
-        </div>
+        <Stack>
+            <TextField
+                id="outlined-basic"
+                onChange={updatePrompt}
+                label="Enter your prompt"
+                variant="outlined"
+                multiline={true}
+                sx={
+                    {
+                        "maxWidth": "600px",
+                    }
+                } />
+            <Button
+                onClick={submitPrompt}
+                variant="contained"
+                sx={
+                    {
+                        "marginTop": "60px",
+                        "height": "60px",
+                        "maxWidth": "200px"
+                    }
+                }
+            >
+                Request Generation
+            </Button>
+        </Stack>
     )
 }
