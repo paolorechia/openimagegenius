04cdbd807daff147b8ae4da9d6d212246d5f8b6e
paolorechia <paolorechia@gmail.com>
2022-09-06 21:41:09 +0200

    Stress test on fanout

--- serverless/openimage-websockets-api-lambda/test_ws_server.py
+++ serverless/openimage-websockets-api-lambda/test_ws_server.py
@@ -3,7 +3,6 @@ import websockets
 import json
 import pytest
 
-
 test_endpoint = "wss://dev.ws-api.openimagegenius.com"
 with open(".ws_secret_pass", "r") as fp:
     ws_secret_pass = fp.read().strip()
@@ -49,3 +48,18 @@ async def test_authorized_request():
         print("Got response:", response)
         j = json.loads(response)
         assert j["message_type"] == "request_accepted"
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_stress_test_jobs():
+    async with websockets.connect(test_endpoint) as websocket:
+        await websocket.send(authorization_event)
+        response = await websocket.recv()
+        assert response == '{"message_type": "authorization", "data": "authorized"}'
+        for i in range(10):
+            await websocket.send(request_event)
+            response = await websocket.recv()
+            print("Got response:", response)
+            j = json.loads(response)
+            assert j["message_type"] == "request_accepted"
