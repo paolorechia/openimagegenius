fa5c17c76b379c5379c7c31ccf70fe3e98faf4db
paolorechia <paolorechia@gmail.com>
2022-09-07 20:50:07 +0200

    Add the original Apache License of SD CPU Engine

--- serverless/private-lambda/installer.py
+++ serverless/private-lambda/installer.py
@@ -4,22 +4,42 @@ import boto3
 import os
 
 s3_client = boto3.client("s3")
+model_files = [
+    "text_encoder.bin",
+    "text_encoder.xml",
+    "unet.bin",
+    "unet.xml",
+    "vae_decoder.bin",
+    "vae_decoder.xml",
+    "vae_encoder.bin",
+    "vae_encoder.xml"
+]
 
 
-def handler(event, context):
-    print("Trying to access EFS filesystem")
-    mount_dir = os.environ['MNT_DIR']
-    lib_dir = os.environ["LIB_DIR"]
-    os.chdir(mount_dir)
-    print("Changed to dir successfully...")
+def install_models(models_dir):
+    try:
+        os.makedirs(models_dir)
+    except FileExistsError:
+        pass
 
-    print("Current dir", os.getcwd())
-    files = os.listdir(mount_dir)
-    print("List of files in directory", files)
+    for model in model_files:
+        print(f"Fetching {model} from S3 Bucket")
+        response = s3_client.get_object(
+            Bucket=os.environ["INSTALLER_BUCKET_NAME"],
+            Key=model
+        )
+        print("Reading byte stream into memory")
+        file_buffer = BytesIO(response["Body"].read())
+        filename = os.path.join(models_dir, model)
+        print(f"Writing buffer to file {filename}")
+        with open(filename, "wb") as fp:
+            fp.write(file_buffer.getbuffer())
 
+
+def install_libraries(lib_dir):
     print("Creating lib directory...")
     try:
-        os.mkdir("./lib")
+        os.makedirs(lib_dir)
     except FileExistsError:
         pass
 
@@ -49,3 +69,19 @@ def handler(event, context):
 
     lib_files = os.listdir(lib_dir)
     print("List of files in lib directory after extracting and writing zip archive to disk:", lib_files)
+
+
+def handler(event, context):
+    print("Trying to access EFS filesystem")
+    mount_dir = os.environ['MNT_DIR']
+    lib_dir = os.environ["LIB_DIR"]
+    models_dir = os.environ["MODELS_DIR"]
+
+    os.chdir(mount_dir)
+    print("Changed to dir successfully...")
+    print("Current dir", os.getcwd())
+    files = os.listdir(mount_dir)
+    print("List of files in directory", files)
+
+    install_libraries(lib_dir)
+    install_models(models_dir)
